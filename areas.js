bb80818bdb8ae854eda4594622ac840df92af402
vvv <t.moll@outlook.com>
2023-12-14 19:35:18 -0500

    Imported clean version of Retro Redrawn.

---
+++ areas.js
@@ -0,0 +1,26 @@
+var marioAreas = [
+  {
+    order: 1,
+    title: "1-1",
+    ident: "1-1",
+    artist: "Arvy",
+    artistImageOverride: "",
+    url: "https://twitter.com/arvydas_b",
+    animation: false,
+    point: {
+      x: 32,
+      y: 1712
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+]
---
+++ implementation.js
@@ -0,0 +1,59 @@
+/*
+*   Retro Redrawn 
+*   -- Implementation Script
+*
+*   Frontend data container of the Redrawn Viewer. 
+*   Contains implementation data specific to a Redrawn project.
+*
+*   by Tyson Moll (vvvvvvv), 2023.
+*
+*/
+
+// Directories
+var artistImgDir = "img/profiles/";
+var artistImgExtension = '.png';  // Image extension for artist images.
+// (having JavaScript self-determine if an image exists without a particular extension is difficult)
+
+/** Content layers in the Redrawn */
+var activeLayerIndex = 0;           // Currently active layer index (and initial index)
+var redrawnLayers = [
+    {
+        name: "smb3",
+        canvasSize: {width: 10000, height: 2096},
+        areas: marioAreas
+    }
+];
+
+/** Biome Data (Screen icons) */
+var biomes = [
+    {
+        name: "Terrace",
+        ident: "terrace",
+        iconId: "grass",
+        color: 'rgb(74 139 89)',
+    },
+    {
+        name: "Underground",
+        ident: "underground",
+        iconId: "fireplace",
+        color: 'rgb(113 104 127)',
+    },
+    {
+        name: "Castle",
+        ident: "castle",
+        iconId: "fort",
+        color: 'rgb(114 79 52)',
+    },
+    {
+        name: "Boss",
+        ident: "boss",
+        iconId: "dark_mode",
+        color: 'rgb(129 28 56)',
+    },
+    {
+        name: "Other",
+        ident: "other",
+        iconId: "dataset",
+        color: 'rgb(94 94 94)',
+    },
+];

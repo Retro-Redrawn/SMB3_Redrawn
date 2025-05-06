7198a190577463c1a29a1880eb505a642422ef6a
Blindeduif <jordi.boin@gmail.com>
2025-05-06 15:01:34 +0200

    removed some jpg pfps setup first little areas for reference edited css.

--- areas.js
+++ areas.js
@@ -1,6 +1,30 @@
 var marioAreas = [
   {
     order: 1,
+    title: "world1",
+    ident: "world1",
+    artist: "Juanito Medina",
+    artistImageOverride: "",
+    url: "https://x.com/juanitomedinart",
+    animation: false,
+    point: {
+      x: 560,
+      y: 100
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 2,
     title: "1-1",
     ident: "1-1",
     artist: "Arvy",
@@ -8,8 +32,200 @@ var marioAreas = [
     url: "https://twitter.com/arvydas_b",
     animation: false,
     point: {
-      x: 32,
-      y: 1712
+      x: 100,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 3,
+    title: "1-2",
+    ident: "1-2",
+    artist: "PawkiUwU",
+    artistImageOverride: "",
+    url: "https://bsky.app/profile/pawkiuwu.bsky.social",
+    animation: false,
+    point: {
+      x: 400,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 5,
+    title: "1-3",
+    ident: "1-3",
+    artist: "GenoCL",
+    artistImageOverride: "",
+    url: "https://genocl.carrd.co/",
+    animation: false,
+    point: {
+      x: 700,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 6,
+    title: "1-4",
+    ident: "1-4",
+    artist: "scooter155",
+    artistImageOverride: "",
+    url: "",
+    animation: false,
+    point: {
+      x: 1000,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 7,
+    title: "world2",
+    ident: "world2",
+    artist: "NekuDemon",
+    artistImageOverride: "",
+    url: "",
+    animation: false,
+    point: {
+      x: 1730,
+      y: 100
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 8,
+    title: "2-1",
+    ident: "2-1",
+    artist: "Marina",
+    artistImageOverride: "",
+    url: "https://cringe.ovh/",
+    animation: false,
+    point: {
+      x: 1400,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 9,
+    title: "2-2",
+    ident: "2-2",
+    artist: "Wortel",
+    artistImageOverride: "",
+    url: "https://cringe.ovh/",
+    animation: false,
+    point: {
+      x: 1700,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 10,
+    title: "2-3",
+    ident: "2-3",
+    artist: "Marmot",
+    artistImageOverride: "",
+    url: "https://cringe.ovh/",
+    animation: false,
+    point: {
+      x: 2000,
+      y: 300
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "horizontal",
+    type: "terrace",
+    zoom: 2,
+    teleporters: [],
+    post_url: ""
+  },
+  {
+    order: 11,
+    title: "2-4",
+    ident: "2-4",
+    artist: "Chren",
+    artistImageOverride: "",
+    url: "https://cringe.ovh/",
+    animation: false,
+    point: {
+      x: 2300,
+      y: 300
     },
     offset: {
       x: null,
